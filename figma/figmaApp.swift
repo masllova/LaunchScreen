23f4fe59cd4db7ce1e17e93b273f38f45363d1df
alexa <allexa.msv@gmail.com>
2023-04-01 02:53:35 +0300

    Initial Commit

---
+++ figma/figmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  figmaApp.swift
+//  figma
+//
+//  Created by Александра Маслова on 01.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct figmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
